ab8fd42fdc3250c926b56ebd9475f319c4f0b728
Rick South <rick@natamus.com>
2024-05-01 16:38:05 +0200

    Humbling Bundle source code for 1.20.6.

---
+++ NeoForge/src/main/java/com/natamus/humblingbundle/neoforge/events/NeoForgeEntityDroppingEvent.java
@@ -0,0 +1,16 @@
+package com.natamus.humblingbundle.neoforge.events;
+
+import com.natamus.humblingbundle.events.EntityDroppingEvent;
+import net.minecraft.world.entity.Entity;
+import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeEntityDroppingEvent {
+	@SubscribeEvent
+	public static void mobItemDrop(LivingDropsEvent e) {
+		Entity entity = e.getEntity();
+		EntityDroppingEvent.mobItemDrop(entity.level(), entity, e.getSource());
+	}
+}
