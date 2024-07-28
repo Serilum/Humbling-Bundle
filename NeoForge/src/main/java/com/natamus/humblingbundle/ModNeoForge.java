e7e5a0acb13becaadac3fb21365784e1996f2fc1
Rick South <rick@natamus.com>
2024-07-28 07:28:31 +0200

    (v2.3) Improved mod icon compression, which lowers the jar file size. [+2]

--- NeoForge/src/main/java/com/natamus/humblingbundle/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/humblingbundle/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.humblingbundle;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.humblingbundle.neoforge.events.NeoForgeEntityDroppingEvent;
 import com.natamus.humblingbundle.util.Reference;
 import net.neoforged.neoforge.common.NeoForge;
@@ -12,6 +13,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
