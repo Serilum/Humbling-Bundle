2788e2f0272ad62a6414fdc3a9979ac69a1b6639
Rick South <rick@natamus.com>
2025-02-12 23:28:56 +0100

    (v2.3) Improved mod icon compression, which lowers the jar file size. [+2]

--- Forge/src/main/java/com/natamus/humblingbundle/ModForge.java
+++ Forge/src/main/java/com/natamus/humblingbundle/ModForge.java
@@ -28,7 +28,7 @@ public class ModForge {
 	}
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
-		MinecraftForge.EVENT_BUS.register(new ForgeEntityDroppingEvent());
+		MinecraftForge.EVENT_BUS.register(ForgeEntityDroppingEvent.class);
 	}
 
 	private static void setGlobalConstants() {
--- NeoForge/src/main/java/com/natamus/humblingbundle/neoforge/events/NeoForgeEntityDroppingEvent.java
+++ NeoForge/src/main/java/com/natamus/humblingbundle/neoforge/events/NeoForgeEntityDroppingEvent.java
@@ -4,9 +4,7 @@ import com.natamus.humblingbundle.events.EntityDroppingEvent;
 import net.minecraft.world.entity.Entity;
 import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeEntityDroppingEvent {
 	@SubscribeEvent
 	public static void mobItemDrop(LivingDropsEvent e) {
