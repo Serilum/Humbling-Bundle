601be3e278bd4b436af873c4d77cb327131e5d3d
Rick South <rick@natamus.com>
2025-10-08 01:31:47 +0200

    Humbling Bundle source code for 1.21.10.

---
+++ Fabric/src/main/java/com/natamus/humblingbundle/ModFabric.java
@@ -0,0 +1,38 @@
+package com.natamus.humblingbundle;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.collective.fabric.callbacks.CollectiveEntityEvents;
+import com.natamus.humblingbundle.events.EntityDroppingEvent;
+import com.natamus.humblingbundle.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectiveEntityEvents.ON_ENTITY_IS_DROPPING_LOOT.register((Level level, Entity entity, DamageSource damageSource) -> {
+			EntityDroppingEvent.mobItemDrop(level, entity, damageSource);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
